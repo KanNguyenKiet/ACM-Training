84a29b2b8952f9e7d509f0debce60cd8d37c2047
lqtungntn <57801454+lqtungntn@users.noreply.github.com>
2021-04-23 01:01:39 +0700

    Update i.cpp

--- NAQC2019/i.cpp
+++ NAQC2019/i.cpp
@@ -2,67 +2,62 @@
 
 using namespace std;
 
-struct cmp{
-	long long dis, cost, id;
-	bool operator< (const cmp &a) const &{
-		return dis < a.dis || (dis == a.dis && cost > a.cost);
-	}
-} d[505];
+long long maxDis = 1e18;
 
-const long long maxDis = 1e18;
-long long n, m, t, s, fix[505];
+long long fix[505], a[505][505], n, m, t, s, b[505][505];
 
 int main(){
-	cin >> n >> m >> t >> s;
-	for (int i = 1; i <= t; i++){
-		int x;
-		cin >> x;
-		fix[x] = 1;
-	}
-	vector<cmp> a[505]; 
-	for (int i = 1; i <= m; i++){
-		long long x, y, z;
-		cin >> x >> y >> z;
-		a[x].push_back({y, z});
-		a[y].push_back({x, z});
-	}
-	priority_queue<cmp> pq;
-	for (int i = 2; i <= n; i++){
-		d[i].dis = maxDis;
-		d[i].cost = 0;
-		d[i].id = i;
-	}
-	d[1].dis = 0;
-	d[1].cost = s;
-	d[1].id = 1;
-	pq.push(d[1]);
-	cmp c;
-	long long c1, c2, c3;
-	while (!pq.empty()){
-		c = pq.top();
-		c1 = c.id;
-		c2 = c.dis;
-		c3 = c.cost;
-		pq.pop();
-		for (int i = 0; i < a[c1].size(); i++){
-			if (c3 - a[c1][i].cost >= 0){
-				if (a[c1][i].dis == n){
-					if (d[a[c1][i].dis].dis > c2 + a[c1][i].cost){
-						d[a[c1][i].dis].dis = c2 + a[c1][i].cost;
-						d[a[c1][i].dis].cost = c3 - a[c1][i].cost;
-					}
-				} else {
-					if (fix[c1] == 1)
-						pq.push({c2 + a[c1][i].cost, s, c1});
-					else
-						pq.push({c2 + a[c1][i].cost, c3 - a[c1][i].cost, c1});
-				}
-			}
-		}	
-	}
-	if (d[n].dis == maxDis){
-		cout << "stuck";
-	} else {
-		cout << d[n].dis;
-	}
+    cin >> n >> m >> t >> s;
+    for (int i = 1; i <= t; i++){
+        int x;
+        cin >> x;
+        fix[x] = 1;
+    }
+    fix[1] = fix[n] = 1;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            a[i][j] = maxDis;
+            b[i][j] = maxDis;
+        }
+        a[i][i] = 0;
+        b[i][i] = 0;
+    }
+    for (int i = 1; i <= m; i++){
+        int x, y, z;
+        cin >> x >> y >> z;
+        a[x][y] = z;
+        a[y][x] = z;
+    }
+    for (int k = 1; k <= n; k++){
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                if (a[i][j] > a[i][k] + a[k][j]){
+                    a[i][j] = a[i][k] + a[k][j];
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            if (i != j){
+                if (fix[i] == 1 && fix[j] == 1 && a[i][j] <= s){
+                    b[i][j] = a[i][j];
+                }
+            }
+        }
+    }
+    for (int k = 1; k <= n; k++){
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                if (b[i][j] > b[i][k] + b[k][j]){
+                    b[i][j] = b[i][k] + b[k][j];
+                }
+            }
+        }
+    }
+    if (b[1][n] == maxDis){
+        cout << "stuck";
+    } else {
+        cout << b[1][n];
+    }
 }
